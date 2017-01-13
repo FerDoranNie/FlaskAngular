68fe62bc82710413219865a37395a438eac81dd1
FerDoranNie <fertimjim@gmail.com>
2017-01-13 14:00:57 -0600

    agregando conexión a base de datos

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_mysqldb import MySQL
 app= Flask(__name__)
 """
 esta linea será para la configuración de heroku
@@ -13,6 +14,16 @@ def hello():
 @app.route("/<name>")
 def hello_name(name):
     return "Hola {}!".format(name)
+
+"""
+Agregando la conexión a base de datos
+"""
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL-DATABASE-PASSWORD']='base10Data'
+app.config['MYSQL_DATABASE_DB']= 'flaskBase'
+mysql.init_app(app)
+
 
 if __name__ == '__main__':
     app.run()
